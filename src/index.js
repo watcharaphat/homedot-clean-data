171191d8d7ba6943f1ec3203c97944e140e1d2ca
watcharaphat <watcharaphat.3166@gmail.com>
2018-07-23 09:41:43 +0700

    correct 0 value data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import util from 'util';
 import fs from 'fs';
 import Papa from 'papaparse';
+import PercentileCalculator from 'percentile-calculator';
+// import median from 'median-quickselect';
 
 const readFileAsync = util.promisify(fs.readFile);
 
@@ -24,6 +26,86 @@ async function getData() {
   return data;
 }
 
-getData().then((data) => {
-  console.log(data[0]);
+getData().then((datas) => {
+  const fields = [
+    'landSize',
+    'starting_price',
+    'amount_bedroom',
+    'amount_bathroom',
+    'amount_car_parking',
+    'unit_functional_space_starting_size',
+  ];
+
+  const dataList = {};
+  fields.forEach((field) => {
+    dataList[field] = [];
+  });
+
+  datas.forEach((data) => {
+    fields.forEach((field) => {
+      if (data[field]) dataList[field].push(data[field]);
+    });
+  });
+
+  let datasRemoveOutlier = datas.slice();
+
+  for (const key in dataList) {
+    const cal = new PercentileCalculator(dataList[key]);
+    const q1 = cal.calculateQ1();
+    const q3 = cal.calculateQ3();
+    const iqr = q3 - q1;
+
+    const oMin = q1 - (1.5 * iqr);
+    const oMax = q3 + (1.5 * iqr)
+
+    if (key !== 'amount_car_parking') {
+      datasRemoveOutlier = datasRemoveOutlier.filter(d => d[key] >= oMin && d[key] <= oMax);
+    }
+    // console.log(`${key}, Q1: ${q1}\tQ3: ${q3}, IQR: ${iqr}, oMin: ${oMin}, oMax: ${oMax}`);
+  }
+
+  const cleanedData = datasRemoveOutlier.slice();
+  const dataListR = {};
+  fields.forEach((field) => {
+    dataListR[field] = [];
+  });
+
+  cleanedData.forEach((data) => {
+    fields.forEach((field) => {
+      if (data[field]) dataListR[field].push(data[field]);
+    });
+  });
+
+  // calculate median
+  const median = {};
+  for (const key in dataListR) {
+    const cal = new PercentileCalculator(dataListR[key]);
+
+    median[key] = cal.calculateQ2();
+    console.log(`${key}: ${median[key]}`);
+  }
+
+  cleanedData.forEach((data) => {
+    // for (const key in data) {
+    //   if (!data[key]) {
+    //     data[key] = median[key];
+    //   }
+    // }
+    fields.forEach((key) => {
+      if (!data[key]) {
+        data[key] = median[key];
+      }
+    });
+  });
+
+  console.log(cleanedData[0]);
+
+  const dataCSV = Papa.unparse(cleanedData);
+  console.log('Writing csv.');
+  fs.writeFileSync(
+    '/Users/watcharaphat/project/homedot-clean-data/data/cleaned_item_feature.csv',
+    dataCSV,
+    'utf8',
+  )
+  console.log('Done');
 });
